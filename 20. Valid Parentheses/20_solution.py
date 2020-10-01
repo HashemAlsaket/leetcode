fcae27d4196d1b4913a66a891a2584a0b1d08aba
Hashem Alsaket <hashemalsaket@gmail.com>
2020-10-01 17:10:55 -0500

    20. valid parentheses

---
+++ 20. Valid Parentheses/20_solution.py
@@ -0,0 +1,31 @@
+﻿class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        l = "([{"
+        stack = []
+        
+        s = [x for x in s] # convert to list for easier handling
+        
+        while s:
+            paren = s.pop(0)
+            if paren in l:
+                stack.append(paren)
+            else:
+                if not stack:
+                    return False
+                p = stack.pop()
+                if p=='(':
+                    if paren!=')':
+                        return False
+                if p=='[':
+                    if paren!=']':
+                        return False
+                if p=='{':
+                    if paren!='}':
+                        return False
+        if stack:
+            return False
+        return True
+        
+        
+        
